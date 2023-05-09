ddb77cf683f8824b5b137294269434bff4f865c9
hoainhon <abeesdevjs@gmail.com>
2023-05-09 23:04:00 +0700

    add textfiled

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -3,3 +3,6 @@ export * from './Button';
 
 export { default as Typography } from './Typography';
 export * from './Typography';
+
+export { default as TextField } from './TextField';
+export * from './TextField';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
-import { Button, Typography } from '@packages/components';
+import { Button, TextField, Typography } from '@packages/components';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen w-screen gap-2">
-      <div className="w-full">
-        <Typography variant="h1" color="text.secondary">
-          Heading
-        </Typography>
-        <Typography variant="h2">Heading</Typography>
-        <Typography variant="h3">Heading</Typography>
-        <Typography variant="h4">Heading</Typography>
-        <Typography variant="h5">Heading</Typography>
-        <Typography variant="h6">Heading</Typography>
-        <Typography variant="body1">Heading</Typography>
-        <Typography variant="body2">Heading</Typography>
-
-        <div className="flex flex-col">
-          <Typography variant="subtitle1">Heading</Typography>
-          <Typography variant="subtitle2">Heading</Typography>
-
-          <Typography variant="caption">Heading</Typography>
-        </div>
-        <Typography variant="body2">Heading</Typography>
+      <div className="w-96">
+        <TextField />
       </div>
     </main>
   );
